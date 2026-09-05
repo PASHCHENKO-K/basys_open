3ed79dab4e91808e4e1fbe7e4ff16ab5fb1142a1
PASHCHENKO-K <258014228+PASHCHENKO-K@users.noreply.github.com>
2026-09-05 22:37:30 +1000

    Веб-версия 0.4.11: Приложение называется Datum

--- app/sw.js
+++ app/sw.js
@@ -15,7 +15,7 @@
 */
 
 /** Подставляется сборкой: отпечаток содержимого вывода. */
-const VERSION = "0.4.10+basys-ed609c9bf01ec3c8";
+const VERSION = "0.4.11+basys-4be3d14b8cde7c49";
 
 /** Подставляется сборкой: всё, что должно работать без сети. */
 const PRECACHE = [
@@ -31,7 +31,7 @@ const PRECACHE = [
   "/basys_open/app/(tabs)/system/tickets",
   "/basys_open/app/+not-found",
   "/basys_open/app/_expo/.routes.json",
-  "/basys_open/app/_expo/static/js/web/entry-8a953c9177e498a84490e395caaebd46.js",
+  "/basys_open/app/_expo/static/js/web/entry-ec9cce22be70eed5563a906fa93595f8.js",
   "/basys_open/app/_sitemap",
   "/basys_open/app/apple-touch-icon.png",
   "/basys_open/app/assets/__node_modules/@expo-google-fonts/ibm-plex-mono/400Regular/IBMPlexMono_400Regular.b112f77f65b1766764ee122703ebdcd0.ttf",
